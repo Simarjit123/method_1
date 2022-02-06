13de85dd857bcae313450249594ea654c347848d
Simarjit <simarjassal9@gmail.com>
2022-02-06 01:01:28 -0500

    access modifier example

---
+++ AccessModifiers/AccessExample.java
@@ -0,0 +1,16 @@
+
+public class AccessExample {
+	String name = "Simar";
+	private int rollNo = 5;
+
+	private void information() {
+		System.out.println("My name is Simar");
+		// private members of the class
+	}
+		
+		 private void description() {
+	       System.out.println("ss");
+	      }
+	}
+
+
---
+++ AccessModifiers/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	 public static void main(String[] main){
+	      AccessExample obj=new AccessExample();
+	      
+			System.out.println(obj.name);
+		
+			 
+     
+	      
+	    }
+}
